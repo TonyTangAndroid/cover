0faa72b2e24bde175a18178da316ce871f6097d1
TonyTandAndroid <tonytangandroid@gmail.com>
2020-01-18 16:34:37 -0800

    Enable 100% code coverage

--- app/src/test/java/net/rafaeltoledo/coverage/Pojo2Test.java
+++ app/src/test/java/net/rafaeltoledo/coverage/Pojo2Test.java
@@ -1,17 +1,18 @@
 package net.rafaeltoledo.coverage;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class Pojo2Test {
 
-    @Test
-    public void testPojo() {
-        Pojo2 p = new Pojo2();
+  @Test
+  public void testPojo() {
+    Pojo2 p = new Pojo2();
 
-        p.setThing("String1");
+    p.setThing("String1");
 
-        assertEquals("String1", p.getThing());
-    }
+    assertEquals("String1", p.getThing());
+    assertEquals("String1", p.missedTest());
+  }
 }
